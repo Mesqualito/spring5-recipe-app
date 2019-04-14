4c6ab60ecf67cc3cb4a032af73304bba7583fdfc
Mesqualito <jochen@gebsattel.rocks>
2019-04-14 14:59:42 +0200

    Analog zu 'https://www.udemy.com/spring-framework-5-beginner-to-guru/learn/v4/t/lecture/7496772' (Abschnitt 8, Lektion 147)
    Getestet. Datenbank-Abfrage und Konsolen-Ausgaben zum Test werden erst bei Besuch der Website (http://localhost:8080) durchgeführt
    
    Closes #13

--- src/main/java/com/eigenbaumarkt/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/eigenbaumarkt/spring5recipeapp/controllers/IndexController.java
@@ -1,18 +1,40 @@
 package com.eigenbaumarkt.spring5recipeapp.controllers;
 
+import com.eigenbaumarkt.spring5recipeapp.domain.Category;
+import com.eigenbaumarkt.spring5recipeapp.domain.UnitOfMeasure;
+import com.eigenbaumarkt.spring5recipeapp.repositories.CategoryRepository;
+import com.eigenbaumarkt.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.htm", "/index.html"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Mexican");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
 
         System.out.println("Ein Spring Boot Development Tools reload test, sobald ein Build stattfindet. " +
                 "Dabei werden zwei Classloader eingesetzt: die dependencies werden extra gehandhabt " +
                 "und nicht refreshed.Die Klassen der eigenen Anwendung werden im zweiten Classloader " +
                 "refreshed...");
+
+        System.out.println("Category ID ist: " + categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure ID ist: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/eigenbaumarkt/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/eigenbaumarkt/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,11 @@ package com.eigenbaumarkt.spring5recipeapp.repositories;
 import com.eigenbaumarkt.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    // instead of returning 'null', Java will return the Optional-Type if empty
+    Optional<Category> findByDescription(String description);
+
 }
