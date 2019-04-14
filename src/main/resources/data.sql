dde6735faeb5afd5b811307dc1dee5ec6541d3a0
Mesqualito <jochen@gebsattel.rocks>
2019-04-14 14:42:34 +0200

    Analog zu 'https://www.udemy.com/spring-framework-5-beginner-to-guru/learn/v4/t/lecture/7496760' (Abschnitt 8, Lektion 146)
    - Es wird die default Hibernate naming strategy verwendet (z.B. UnitOfMeasure => unit_of_measure)
    - Table-names und table-columns sollten case-insensitive, manche Datenbanken sind nicht case-sensitive
    
    Closes #12

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+-- data.sql is the Spring JDBC - not the Hibernate - initialisation file
+-- comes from resources to the classpath root while build
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
