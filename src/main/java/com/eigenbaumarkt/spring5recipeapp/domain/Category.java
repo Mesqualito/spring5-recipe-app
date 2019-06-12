ca2a51560d526f4bf2464bdddec3b1030cdf8356
Mesqualito <jochen@gebsattel.rocks>
2019-06-12 18:24:57 +0200

    @Data / lombok: - "org.hibernate.LazyInitializationException: failed to lazily initialize a collection of role: com.eigenbaumarkt.spring5recipeapp.domain.Category.recipes, could not initialize proxy - no Session" - gelöst mit "fetch = FetchType.EAGER" bei @OneToMany und @ManyToMany-Beziehungen mit Hibernate - unter "https://www.udemy.com/spring-framework-5-beginner-to-guru/learn/lecture/7497552" (Abschnitt 9, Lektion 173) wird der "java.lang.StackOverFlowError" mit @EqualsAndHashCode zusätzlich zu @Data (beides lombok) gelöst. Siehe dazu: https://stackoverflow.com/questions/22821695/how-to-fix-hibernate-lazyinitializationexception-failed-to-lazily-initialize-a

--- src/main/java/com/eigenbaumarkt/spring5recipeapp/domain/Category.java
+++ src/main/java/com/eigenbaumarkt/spring5recipeapp/domain/Category.java
@@ -17,7 +17,7 @@ public class Category {
 
     private String description;
 
-    @ManyToMany(mappedBy = "categories")
+    @ManyToMany(mappedBy = "categories", fetch = FetchType.EAGER)
     private Set<Recipe> recipes;
 
 }
