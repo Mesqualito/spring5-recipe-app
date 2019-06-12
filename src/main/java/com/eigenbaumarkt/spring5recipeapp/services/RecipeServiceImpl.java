dac403c1522436d5f75f913100361b3f150462f6
Mesqualito <jochen@gebsattel.rocks>
2019-06-12 17:29:52 +0200

    Analog zu 'https://www.udemy.com/spring-framework-5-beginner-to-guru/learn/lecture/7497524' (Abschnitt 9, Lektion 170)
    BUGGY - not compiling.
    Ggf. gradle => lombok ?

--- src/main/java/com/eigenbaumarkt/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/eigenbaumarkt/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.eigenbaumarkt.spring5recipeapp.services;
 
 import com.eigenbaumarkt.spring5recipeapp.domain.Recipe;
 import com.eigenbaumarkt.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,6 +21,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm the service!");
         Set<Recipe> recipeSet = new HashSet<>();
 
         // since Java 8
