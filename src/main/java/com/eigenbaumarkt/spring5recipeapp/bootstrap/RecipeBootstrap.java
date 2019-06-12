ba1a22520292f6690a1975f78b1535efa31afd49
Mesqualito <jochen@gebsattel.rocks>
2019-06-12 18:40:33 +0200

    Ein anderer Weg, um die Probleme mit lazy transaction zu umgehen: https://www.udemy.com/spring-framework-5-beginner-to-guru/learn/lecture/7497552 (ab ca. min. 4:45)

--- src/main/java/com/eigenbaumarkt/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/eigenbaumarkt/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -31,6 +32,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     // java.util.List is an iterable
     // will save everything coming out of the repositories
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
         log.debug("Loading Bootstrap Data!");
