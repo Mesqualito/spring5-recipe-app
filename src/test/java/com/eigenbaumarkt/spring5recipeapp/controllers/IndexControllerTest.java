b203c95a377f0c99aef633be7817cfb8b5b966dc
Mesqualito <jochen@gebsattel.rocks>
2019-07-29 21:09:33 +0200

    analog zu 'https://www.udemy.com/spring-framework-5-beginner-to-guru/learn/lecture/7497718' (Abschnitt 10, Video 183) Doing deeper tests with Mockito Argument Capture

--- src/test/java/com/eigenbaumarkt/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/eigenbaumarkt/spring5recipeapp/controllers/IndexControllerTest.java
@@ -1,17 +1,20 @@
 package com.eigenbaumarkt.spring5recipeapp.controllers;
 
+import com.eigenbaumarkt.spring5recipeapp.domain.Recipe;
 import com.eigenbaumarkt.spring5recipeapp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
 
@@ -31,16 +34,30 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() throws Exception {
+
+        // given
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe1.setId(2L);
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // when
+        when(recipeService.getRecipes()).thenReturn(recipes);
         String viewName = controller.getIndexPage(model);
 
+        // then
         // Unit test of class IndexController and its method getIndexPage()
         assertEquals("index", viewName);
 
         // test interaction on Mock-Objects
         verify(recipeService, times(1)).getRecipes();
-        // see IndexController-class to find the attributes name and compare it with Mockito static method anySet().
-        // "Mockito extends ArgumentMatchers so to get access to all matchers just import Mockito class statically."
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
-
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
